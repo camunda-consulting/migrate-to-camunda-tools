eefcd5f21704cd07754073cff1ef3dff8f8ba53b
Joe Pappas <joe.pappas@me.com>
2020-03-11 16:38:52 -0400

    Typo fix

--- Oracle/Create BPMN from Oracle BPMN/src/test/java/org/camunda/bpmn/generator/BPMNGenFromOracleTest.java
+++ Oracle/Create BPMN from Oracle BPMN/src/test/java/org/camunda/bpmn/generator/BPMNGenFromOracleTest.java
@@ -8,6 +8,6 @@ public class BPMNGenFromOracleTest {
     public void main() throws Exception {
         String[] args = {"src/main/resources/OracleExport.bpmn", "src/main/resources/OracleExportConverted.bpmn"};
         BPMNGenFromOracle.main(args);
-        System.out.println("Please visually compare the original process in IBM BPM Process Designer (picture provided) with the generated output in Camunda Modeler");
+        System.out.println("Please visually compare the original process in Oracle BPM (picture provided) with the generated output in Camunda Modeler");
     }
 }
