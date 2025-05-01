5a719a8f284b815b4ec24ad4afe1c0df30962dec
Joe Pappas <joe.pappas@me.com>
2025-05-01 10:34:03 -0400

    Added C8 support for IBM BPMN export

---
+++ IBM/create diagram from exported BPMN/create Camunda 8 BPMN from exported IBM BPMN/src/main/java/org/camunda/create/bpmndi/DrawFlow.java
@@ -0,0 +1,54 @@
+package org.camunda.create.bpmndi;
+
+import io.camunda.zeebe.model.bpmn.instance.BaseElement;
+import io.camunda.zeebe.model.bpmn.instance.bpmndi.BpmnEdge;
+import io.camunda.zeebe.model.bpmn.instance.bpmndi.BpmnPlane;
+import io.camunda.zeebe.model.bpmn.instance.di.Waypoint;
+import org.camunda.bpm.model.xml.ModelInstance;
+import org.w3c.dom.Element;
+
+import java.util.HashMap;
+
+public class DrawFlow {
+
+    public static BpmnPlane drawFlow(BpmnPlane plane, ModelInstance modelInstance, Element sfElement, HashMap<String, SequenceReferencePoints> refPoints) {
+
+        String sourceRef = sfElement.getAttribute("sourceRef");
+        String targetRef = sfElement.getAttribute("targetRef");
+
+        SequenceReferencePoints srp = refPoints.get(sourceRef);
+
+        if(srp != null) {
+            double xExit = srp.getXExit();
+            double yExit = srp.getYExit();
+
+            srp = refPoints.get(targetRef);
+            double xEntry = srp.getXEntry();
+            double yEntry = srp.getYEntry();
+
+            BpmnEdge bpmnEdge = modelInstance.newInstance(BpmnEdge.class);
+            BaseElement element = modelInstance.getModelElementById(sfElement.getAttribute("id"));
+            bpmnEdge.setBpmnElement(element);
+            double xExitWaypoint = new Double(xExit);
+            double yExitWaypoint = new Double(yExit);
+            Waypoint wp = modelInstance.newInstance(Waypoint.class);
+            wp.setX(xExitWaypoint);
+            wp.setY(yExitWaypoint);
+            bpmnEdge.addChildElement(wp);
+
+            double xEntryWaypoint = new Double(xEntry);
+            double yEntryWaypoint = new Double(yEntry);
+            wp = modelInstance.newInstance(Waypoint.class);
+            wp.setX(xEntryWaypoint);
+            wp.setY(yEntryWaypoint);
+
+            bpmnEdge.addChildElement(wp);
+
+            plane.addChildElement(bpmnEdge);
+        } else {
+            System.out.println("Sequence reference not found " + sourceRef);
+        }
+
+        return plane;
+    }
+}
